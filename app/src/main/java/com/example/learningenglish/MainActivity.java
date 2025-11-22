fb871f285d20d16d17e2b40bc3206e590b0eef42
SoulsHunter352 <vovamaiorov11@gmail.com>
2025-11-22 13:16:53 +0300

    Набросок дизайна настроек

--- app/src/main/java/com/example/learningenglish/MainActivity.java
+++ app/src/main/java/com/example/learningenglish/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.learningenglish;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private NumberPicker hoursPicker;
+    private NumberPicker minutesPicker;
+    private NumberPicker secondsPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        hoursPicker = findViewById(R.id.hours_picker);
+        minutesPicker = findViewById(R.id.minutes_picker);
+        secondsPicker = findViewById(R.id.seconds_picker);
+    }
+
+    protected void configureNumberPickers(){
+
     }
 }
